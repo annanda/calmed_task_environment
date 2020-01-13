b2dab6786b71b6c41a3a0567714319ed767f57c5
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-01-13 09:56:05 +0000

    adding first task page.

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def index_page():
     return render_template('index.html', **info)
 
 
+@app.route('/first_task', methods=['GET'])
+def first_task():
+    return render_template('first_task.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=90)

b36582090ff334cb1ba04259348eb08e07c8be5f
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-01-13 09:26:00 +0000

    Creating the initial files of the project.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="./templates")
+
+
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/', methods=['GET'])
+def index_page():
+    info = {}
+    return render_template('index.html', **info)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=90)

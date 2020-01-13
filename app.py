6c328714d75cc7164a3b6ced9d54c5b455a01531
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-01-13 10:47:34 +0000

    Finishing the first version with, 4 tasks (all video content), calming content between the tasks (all video content) and initial and ending pages. Links connecting pages.

--- app.py
+++ app.py
@@ -23,5 +23,42 @@ def calming_content():
     return render_template('calming_content.html')
 
 
+@app.route('/calming_content_2', methods=['GET'])
+def calming_content_2():
+    return render_template('calming_content_2.html')
+
+
+@app.route('/calming_content_3', methods=['GET'])
+def calming_content_3():
+    return render_template('calming_content_3.html')
+
+
+@app.route('/calming_content_4', methods=['GET'])
+def calming_content_4():
+    return render_template('calming_content_4.html')
+
+
+@app.route('/second_task', methods=['GET'])
+def second_task():
+    return render_template('second_task.html')
+
+
+@app.route('/third_task', methods=['GET'])
+def third_task():
+    # return "this is the third task"
+    return render_template('third_task.html')
+
+
+@app.route('/forth_task', methods=['GET'])
+def forth_task():
+    # return "this is the forth task"
+    return render_template('forth_task.html')
+
+
+@app.route('/ending', methods=['GET'])
+def ending():
+    return render_template('ending.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=90)

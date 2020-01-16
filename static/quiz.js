67a913859ffe46f8030a66e2a4883d80cb145153
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-01-16 11:42:27 +0000

    Starting to create the javascript quiz to be used as worksheet for task 3.

---
+++ static/quiz.js
@@ -0,0 +1,151 @@
+window.onload = function () {
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+
+
+    const myQuestions = [
+        {
+            question: "Who is the strongest?",
+            answers: {
+                a: "Superman",
+                b: "The Terminator",
+                c: "Waluigi, obviously"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What is the best site ever created?",
+            answers: {
+                a: "SitePoint",
+                b: "Simple Steps Code",
+                c: "Trick question; they're both the best"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Where is Waldo really?",
+            answers: {
+                a: "Antarctica",
+                b: "Exploring the Pacific Ocean",
+                c: "Sitting in a tree",
+                d: "Minding his own business, so stop asking"
+            },
+            correctAnswer: "d"
+        }
+    ];
+
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach(
+            (currentQuestion, questionNumber) => {
+
+                // we'll want to store the list of answer choices
+                const answers = [];
+
+                // and for each available answer...
+                for (letter in currentQuestion.answers) {
+
+                    // ...add an HTML radio button
+                    answers.push(
+                        `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+                    );
+                }
+
+                // add this question and its answers to the output
+                output.push(
+                    `<div class="slide">
+                        <div class="question"> ${currentQuestion.question} </div>
+                        <div class="answers"> ${answers.join('')} </div>
+                    </div>`
+                );
+            }
+        );
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = 'input[name=question' + questionNumber + ']:checked';
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
+    }
+
+    // display quiz right away
+    buildQuiz();
+
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        } else {
+            previousButton.style.display = 'inline-block';
+        }
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        } else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    showSlide(0);
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+
+    // on submit, show results
+    submitButton.addEventListener('click', showResults);
+}

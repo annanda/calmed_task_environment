e45ac42364a473f8a3c454ea9a1eb7663c33dd4d
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-01-28 07:38:56 +0000

    Adding the mood pages into the task environment flow.

--- static/mood.js
+++ static/mood.js
@@ -19,41 +19,44 @@ function make_request(text, task, method = "post") {
 
 function get_url_vars() {
     let vars = {};
-    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
 }
+
 let next_page = '';
+let task = ''
 window.onload = function () {
-    let task = get_url_vars()["task"];
-    if (task == 'first_task'){
+    task = get_url_vars()["task"];
+    let mood = ''
+    if (task == 'first_task') {
         next_page = 'calming_content';
     }
-    if (task == 'second_task'){
+    if (task == 'second_task') {
         next_page = 'calming_content_2';
     }
-    if (task == 'third_task'){
+    if (task == 'third_task') {
         next_page = 'calming_content_3';
     }
-    if (task == 'forth_task'){
+    if (task == 'forth_task') {
         next_page = 'calming_content_4';
     }
     document.getElementById("happy_mood").onclick = function () {
-        let mood = 'happy_mood';
+        mood = 'happy_mood';
         make_request(mood, task);
     }
     document.getElementById("neutral_mood").onclick = function () {
-        let mood = 'neutral_mood';
+        mood = 'neutral_mood';
         make_request(mood, task);
     }
     document.getElementById("unhappy_mood").onclick = function () {
-        let mood = 'unhappy_mood';
+        mood = 'unhappy_mood';
         make_request(mood, task);
     }
 }
 
 let time_to_move = get_url_vars()["time_next"];
 setTimeout(function () {
-    window.location.href = next_page;
+    // window.location.href = next_page;
 }, time_to_move);

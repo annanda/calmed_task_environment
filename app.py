fc18f1aacee9e419a3fd22baec71372c4f2a9ec1
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-03-30 11:43:53 +0100

    Saving the starting and ending time of AV recording into a DB.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, json
-from models import db, TaskTime, MoodTime
+from models import db, TaskTime, MoodTime, AVRecordTime
 from datetime import datetime
 from conf import TIME_ON_PAGE_TASK, TIME_ON_PAGE_CALMING, TIME_ON_PAGE_TO_READ, TIME_ON_PAGE_MOOD, INDEX_BG_IMAGE, \
     CALMING_VIDEO_1, CALMING_VIDEO_2, CALMING_VIDEO_3, CALMING_VIDEO_4, GREEN_ZONE_VIDEO, BLUE_ZONE_VIDEO
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///saved-times.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 RECORD_AV_PROCESS = None
+STARTING_TIME_AV_RECORD = None
+ENDING_TIME_AV_RECORD = None
+
 
 @app.route('/', methods=['GET'])
 def index_page():
@@ -19,8 +22,8 @@ def index_page():
     return render_template('index.html', **info, image_bg=INDEX_BG_IMAGE)
 
 
-def adding_db_task_timestamp(task, timestamp):
-    db_new_entry = TaskTime(task=task, timestamp=timestamp)
+def adding_db_task_timestamp(page, timestamp):
+    db_new_entry = TaskTime(page=page, timestamp=timestamp)
     db.session.add(db_new_entry)
     db.session.commit()
 
@@ -60,7 +63,7 @@ def forth_task():
 @app.route('/calming_content', methods=['GET'])
 def calming_content():
     timestamp = datetime.now()
-    adding_db_task_timestamp('calming_content', timestamp)
+    adding_db_task_timestamp('calming_content_1', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_CALMING)
     return render_template('calming_content.html', time_on_page=time_on_page, calming_video=CALMING_VIDEO_1)
 
@@ -68,7 +71,7 @@ def calming_content():
 @app.route('/calming_content_2', methods=['GET'])
 def calming_content_2():
     timestamp = datetime.now()
-    adding_db_task_timestamp('calming_content', timestamp)
+    adding_db_task_timestamp('calming_content_2', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_CALMING)
     return render_template('calming_content_2.html', time_on_page=time_on_page, calming_video=CALMING_VIDEO_2)
 
@@ -76,7 +79,7 @@ def calming_content_2():
 @app.route('/calming_content_3', methods=['GET'])
 def calming_content_3():
     timestamp = datetime.now()
-    adding_db_task_timestamp('calming_content', timestamp)
+    adding_db_task_timestamp('calming_content_3', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_CALMING)
     return render_template('calming_content_3.html', time_on_page=time_on_page, calming_video=CALMING_VIDEO_3)
 
@@ -84,61 +87,83 @@ def calming_content_3():
 @app.route('/calming_content_4', methods=['GET'])
 def calming_content_4():
     timestamp = datetime.now()
-    adding_db_task_timestamp('calming_content', timestamp)
+    adding_db_task_timestamp('calming_content_4', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_CALMING)
     return render_template('calming_content_4.html', time_on_page=time_on_page, calming_video=CALMING_VIDEO_4)
 
 
 @app.route('/ending', methods=['GET'])
 def ending():
+    # this is the timestamp the video record will end
     timestamp = datetime.now()
     adding_db_task_timestamp('ending_page', timestamp)
     global RECORD_AV_PROCESS
     RECORD_AV_PROCESS.communicate(b'q')
+    global ENDING_TIME_AV_RECORD
+    ENDING_TIME_AV_RECORD = timestamp
+    if STARTING_TIME_AV_RECORD is not None:
+        store_av_record_time(STARTING_TIME_AV_RECORD, ENDING_TIME_AV_RECORD)
     # RECORD_AV_PROCESS.send_signal(signal.SIGINT)
     return render_template('ending.html')
 
 
 @app.route('/game', methods=['GET'])
 def game():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('game_page', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TASK)
     return render_template('game.html', time_on_page=time_on_page)
 
 
 @app.route('/end_game', methods=['GET'])
 def end_game():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('ending_game_page', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     return render_template('end_game.html', time_on_page=time_on_page)
 
 
 @app.route('/schedule', methods=['GET'])
 def schedule():
+    # this is the timestamp the record video will start
+    timestamp = datetime.now()
+    adding_db_task_timestamp('schedule_page_initial', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     global RECORD_AV_PROCESS
     RECORD_AV_PROCESS = subprocess.Popen(['python', 'audio_video_record.py'], stdin=subprocess.PIPE)
+    global STARTING_TIME_AV_RECORD
+    STARTING_TIME_AV_RECORD = timestamp
     return render_template('schedule.html', time_on_page=time_on_page)
 
 
 @app.route('/schedule_after_1', methods=['GET'])
 def schedule_after_1():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('schedule_page_after_task_1', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     return render_template('schedule_after_1.html', time_on_page=time_on_page)
 
 
 @app.route('/schedule_after_2', methods=['GET'])
 def schedule_after_2():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('schedule_page_after_task_2', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     return render_template('schedule_after_2.html', time_on_page=time_on_page)
 
 
 @app.route('/schedule_after_3', methods=['GET'])
 def schedule_after_3():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('schedule_page_after_task_3', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     return render_template('schedule_after_3.html', time_on_page=time_on_page)
 
 
 @app.route('/schedule_ending', methods=['GET'])
 def schedule_ending():
+    timestamp = datetime.now()
+    adding_db_task_timestamp('schedule_page_ending', timestamp)
     time_on_page = json.dumps(TIME_ON_PAGE_TO_READ)
     return render_template('schedule_ending.html', time_on_page=time_on_page)
 
@@ -168,6 +193,12 @@ def store_mood():
     db.session.commit()
     return 'mood saved on DB'
     # return 'testing POST'
+
+
+def store_av_record_time(start_time, end_time):
+    db_entry = AVRecordTime(timestamp_start=start_time, timestamp_end=end_time)
+    db.session.add(db_entry)
+    db.session.commit()
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 
 class TaskTime(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(80), unique=False, nullable=False)
+    page = db.Column(db.String(80), unique=False, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-        return f'<id: {self.id}, task:{self.task}, timestamp:{self.timestamp}>'
+        return f'<id: {self.id}, task:{self.page}, timestamp:{self.timestamp}>'
 
 
 class MoodTime(db.Model):
@@ -20,3 +20,12 @@ class MoodTime(db.Model):
 
     def __repr__(self):
         return f'<id: {self.id}, mood:{self.mood}, task:{self.task}, timestamp:{self.timestamp}>'
+
+
+class AVRecordTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp_start = db.Column(db.DateTime, nullable=False)
+    timestamp_end = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<id: {self.id}, start time:{self.timestamp_start}, end time:{self.timestamp_end}>'
